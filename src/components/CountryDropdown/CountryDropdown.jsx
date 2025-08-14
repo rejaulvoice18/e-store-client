7547fdf33f214757f9ab8756b729b0e4a5f53f22
rejaulvoice18 <rejaulvoice@gmail.com>
2025-08-14 07:14:46 +0100

    Implemented a Modal for country drop down

--- src/components/CountryDropdown/CountryDropdown.jsx
+++ src/components/CountryDropdown/CountryDropdown.jsx
@@ -1,16 +1,65 @@
-import { Button } from "@mui/material";
+import { Button, Dialog } from "@mui/material";
+import { useState } from "react";
 import { FaAngleDown } from "react-icons/fa";
+import { IoIosSearch } from "react-icons/io";
+import { MdClose } from "react-icons/md";
 
 const CountryDropdown = () => {
+
+    const [isOpenModal, setIsOpenModal] = useState(false)
     return (
         <div>
-            <Button className='countryDrop'>
+            <Button className='countryDrop' onClick={()=>setIsOpenModal(true)}>
                 <div className='info d-flex flex-column'>
                     <span className="label">Your Location</span>
                     <span className="name">India</span>
                 </div>
                 <span className="ml-auto"><FaAngleDown /></span>
             </Button>
+
+            {/* Pop Up Modal */}
+            <Dialog open={isOpenModal} className="locationModal">
+                <h4 className="mb-0">Choose your Delivery Location</h4>
+                <p>Enter your address and we will specify the offer for your area.</p>
+                <Button className="close_" onClick={()=> setIsOpenModal(false)}><MdClose /></Button>
+                <div className='headerSearch w-100'>
+                    <input type="text" placeholder='Search your area' />
+                    <Button><IoIosSearch /></Button>
+                </div>
+
+                <ul className="countryList mt-3">
+                    <li><Button>Bangladesh</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Portugal</Button></li>
+                    <li><Button>Franch</Button></li>
+                    <li><Button>Spain</Button></li>
+                    <li><Button>Germany</Button></li>
+                    <li><Button>Bangladesh</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Portugal</Button></li>
+                    <li><Button>Franch</Button></li>
+                    <li><Button>Spain</Button></li>
+                    <li><Button>Germany</Button></li>
+                    <li><Button>Bangladesh</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Portugal</Button></li>
+                    <li><Button>Franch</Button></li>
+                    <li><Button>Spain</Button></li>
+                    <li><Button>Germany</Button></li>
+                    <li><Button>Bangladesh</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Portugal</Button></li>
+                    <li><Button>Franch</Button></li>
+                    <li><Button>Spain</Button></li>
+                    <li><Button>Germany</Button></li>
+                    <li><Button>Bangladesh</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Portugal</Button></li>
+                    <li><Button>Franch</Button></li>
+                    <li><Button>Spain</Button></li>
+                    <li><Button>Germany</Button></li>
+                </ul>
+            </Dialog>
         </div>
     );
 };
